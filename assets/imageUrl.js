7d98b28615535fd24e78294b6e2239134bc01d54
BG-DARWIN <144324933+DARWIN-OFFICIAL@users.noreply.github.com>
2025-05-24 01:06:24 +0700

    Add files via upload
    Signed-off-by: BG-DARWIN <144324933+DARWIN-OFFICIAL@users.noreply.github.com>

---
+++ assets/imageUrl.js
@@ -0,0 +1,6 @@
+const AlectoImg = [
+        "https://pomf2.lain.la/f/a0epm44t.jpg",
+        "https://pomf2.lain.la/f/vhug0p.jpg",
+        "https://pomf2.lain.la/f/sxi9acke.jpg",
+        "https://pomf2.lain.la/f/36k8t27u.jpg" // Replace with the actual second image URL
+    ];
---
+++ assets/pricingData.js
@@ -0,0 +1,18 @@
+// Data Harga untuk tiap game
+const PUBGPrice = [
+    {duration: "1 Day", priceUSD: "$1", priceIDR: "Rp 15,000", priceMR: "MR1"},
+    {duration: "1 Week", priceUSD: "$5", priceIDR: "Rp 75,000", priceMR: "MR5"},
+    {duration: "1 Month", priceUSD: "$15", priceIDR: "Rp 225,000", priceMR: "MR15"}
+];
+
+const MobileLegendsPrice = [
+    {duration: "3 Days", priceUSD: "$2", priceIDR: "Rp 30,000", priceMR: "MR2"},
+    {duration: "1 Week", priceUSD: "$6", priceIDR: "Rp 90,000", priceMR: "MR6"},
+    {duration: "1 Month", priceUSD: "$18", priceIDR: "Rp 270,000", priceMR: "MR18"}
+];
+
+const FreeFire = [
+    {duration: "1 Day", priceUSD: "$1.5", priceIDR: "Rp 22,000", priceMR: "MR1.5"},
+    {duration: "1 Week", priceUSD: "$7", priceIDR: "Rp 105,000", priceMR: "MR7"},
+    {duration: "1 Month", priceUSD: "$20", priceIDR: "Rp 300,000", priceMR: "MR20"}
+];
